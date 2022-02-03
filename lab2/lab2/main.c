e4cdcdc5320c7df405bb7ba87163b4a918309d14
elvfor <elvfor-0@student.ltu.se>
2022-02-03 08:46:10 +0100

    starting now

---
+++ lab2/lab2/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab2.c
+ *
+ * Created: 2022-02-03 08:35:13
+ * Author : elvir
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
